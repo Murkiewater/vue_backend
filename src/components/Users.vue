89b46bc5d9e83f58e41808f67599c05b659d167e
Toni <tonihugo@student.21-school.ru>
2026-03-30 12:03:03 +0500

    in progress

---
+++ src/components/Users.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="table">
+    <h1>работаеттттт</h1>
+  </div>
+</template>
---
+++ src/router.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Friends from '@/components/Friends.vue';
+import Groups from '@/components/Groups.vue';
+import Users from '@/components/Users.vue';
+const routes = [
+    {
+        path: '/',
+        component: Home,
+    },
+    {
+        path: '/groups',
+        component: Groups,
+    },
+    {
+        path: '/users',
+        component: Users,
+    },
+    {
+        path: '/friends',
+        component: Friends,
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
